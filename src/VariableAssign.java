b33b630c7dee5d81cbfd507b2fd664e68d7b05ca
Kim Levin <kbussie1@montgomerycollege.edu>
2020-04-14 13:01:00 -0400

    my first commit

---
+++ src/VariableAssign.java
@@ -0,0 +1,11 @@
+public class VariableAssign {
+    public static void main(String[] args){
+        int testScore = 50;
+        float priceTag = 24.99f;
+        String userName = "Bobby Johnson";
+
+        System.out.println("Test Score initialized is: " + testScore);
+        System.out.println("Price tag: " + priceTag);
+        System.out.println("Username: " + userName);
+    }
+}
